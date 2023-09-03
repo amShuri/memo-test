50e60f95a152b97c8fef430e029a6a5bf70d2bdf
amShuri <106229595+amShuri@users.noreply.github.com>
2023-09-03 18:05:02 -0300

    Implement user input handling and tile matching logic

--- main.js
+++ main.js
@@ -1,4 +1,46 @@
 const $gameboard = document.querySelector('#gameboard');
+const gameTiles = [];
+
+$gameboard.onclick = function (e) {
+  handleUserTileClick(e);
+
+  if (gameTiles.length == 2) {
+    disableUserInput();
+    handleTilePair(gameTiles[0], gameTiles[1]);
+    setTimeout(() => {
+      enableUserInput();
+      gameTiles.splice(0);
+    }, 1000);
+  }
+};
+
+function handleUserTileClick(e) {
+  if (e.target.tagName != 'BUTTON' || e.target == gameTiles[0]) return;
+  e.target.classList.remove('hidden-tile');
+  gameTiles.push(e.target);
+}
+
+function handleTilePair(tileOne, tileTwo) {
+  if (tileOne.className == tileTwo.className) {
+    setTimeout(() => {
+      tileOne.classList.add('matched-tile');
+      tileTwo.classList.add('matched-tile');
+    }, 1000);
+  } else {
+    setTimeout(() => {
+      tileOne.classList.add('hidden-tile');
+      tileTwo.classList.add('hidden-tile');
+    }, 1000);
+  }
+}
+
+function disableUserInput() {
+  document.querySelectorAll('.hidden-tile').forEach((tile) => (tile.disabled = true));
+}
+
+function enableUserInput() {
+  document.querySelectorAll('.hidden-tile').forEach((tile) => (tile.disabled = false));
+}
 
 function generateTiles() {
   const pairOfTiles = 6;
@@ -11,7 +53,7 @@ function generateTiles() {
     const $tile = document.createElement('button');
 
     $tileWrapper.classList.add('col', 'tile');
-    $tile.classList.add(tempArray[i]);
+    $tile.classList.add('hidden-tile', tempArray[i]);
 
     $gameboard.appendChild($tileWrapper);
     $tileWrapper.appendChild($tile);
