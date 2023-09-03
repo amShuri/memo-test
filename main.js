6fad55168900de64e96c353953cb4117bd8e798b
amShuri <106229595+amShuri@users.noreply.github.com>
2023-09-03 15:19:03 -0300

    Update tile generation with randomization
    Previously, this function was applying the color classes in a linear manner, so the same tiles were on the same spot every time.
    I solved this issue by creating a new array that holds twice the amount of colors, shuffling it and then using its content to assign the classes, without altering the original array in any way.

--- main.js
+++ main.js
@@ -3,16 +3,27 @@ const $gameboard = document.querySelector('#gameboard');
 function generateTiles() {
   const pairOfTiles = 6;
   const tileColors = ['blue', 'green', 'orange', 'pink', 'purple', 'red'];
+  const tempArray = [...tileColors, ...tileColors];
+  shuffleArray(tempArray);
 
   for (let i = 0; i < pairOfTiles * 2; i += 1) {
     const $tileWrapper = document.createElement('div');
     const $tile = document.createElement('button');
 
     $tileWrapper.classList.add('col', 'tile');
-    $tile.classList.add(tileColors[i % tileColors.length]);
+    $tile.classList.add(tempArray[i]);
 
     $gameboard.appendChild($tileWrapper);
     $tileWrapper.appendChild($tile);
+  }
+}
+
+function shuffleArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    const tempIndex = array[i];
+    array[i] = array[randomIndex];
+    array[randomIndex] = tempIndex;
   }
 }
 
