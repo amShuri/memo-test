4e3b98d92a4a4a29ab4b4e4a00510e0e983e3a6e
amShuri <106229595+amShuri@users.noreply.github.com>
2023-09-15 18:17:41 -0300

    Update Cypress test by replacing hard-coded values with variables

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -41,14 +41,14 @@ context('Memotest', () => {
     it('works with non-matching tiles', () => {
       cy.get(tilePairsList[0][0]).click();
       cy.get(tilePairsList[3][0]).click();
-      cy.get('.tile').should('have.length', 12);
+      cy.get('.tile').should('have.length', AMOUNT_TILES);
     });
 
     it('completes the game successfully and shows the gameover message', () => {
-      cy.get('.tile').should('have.length', 12);
-      tilePairsList.forEach((par) => {
-        cy.get(par[0]).click();
-        cy.get(par[1]).click();
+      cy.get('.tile').should('have.length', AMOUNT_TILES);
+      tilePairsList.forEach((tilePair) => {
+        cy.get(tilePair[0]).click();
+        cy.get(tilePair[1]).click();
       });
       cy.get('.tile').should('have.length', 0);
       cy.get('#gameboard').should('not.be.visible');
